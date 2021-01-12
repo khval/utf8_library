f1963673ec13e472b6db6a2ed67dad0d3a72ce6d
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2021-01-12 19:25:40 +0100

    Interfaces moved out of libBase.

--- UTF8_files/libbase.h
+++ UTF8_files/libbase.h
@@ -1,15 +1,20 @@
+
+struct Library *DOS_base;
+struct Library *Graphics_base;
+struct Library *Diskfont_base;
+
+#ifdef __amigaos4__
+struct ExecIFace *IExec;
+struct DOSIFace *IDOS;
+struct GraphicsIFace *IGraphics;
+struct DiskfontIFace *IDiskfont;
+#endif
+
 struct _Library
 {
 	struct Library libNode;
 	BPTR segList;
     // If you need more data fields, add them here 
-	struct Library *DOS_base;
-	struct Library *Graphics_base;
-	struct Library *Diskfont_base;
-	struct ExecIFace *IExec;
-	struct DOSIFace *IDOS;
-	struct GraphicsIFace *IGraphics;
-	struct DiskfontIFace *IDiskfont;
 	int MainCnt;
 };
 
