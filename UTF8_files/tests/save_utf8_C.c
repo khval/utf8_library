ada1226058f00973f08651f19cf9f31b08b3bdee
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2021-01-17 14:49:38 +0100

    Test save_utf8 rewritten.

--- UTF8_files/tests/save_utf8_C.c
+++ UTF8_files/tests/save_utf8_C.c
@@ -3,6 +3,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define __USE_INLINE__
+
 #include <stdio.h>
 #include <string.h>
 #include <proto/exec.h>
@@ -20,8 +22,8 @@ uint32 codeset;
 #define DefineLib( a ) struct Library * a ## _base; struct a ## IFace *I ## a;
 
 #define CloseLIB( name ) \
-	if ( I##name) IExec -> DropInterface( (struct Interface *) I##name ); \
-	if ( name  ## _base) IExec -> CloseLibrary( name ## _base); \
+	if ( I##name) DropInterface( (struct Interface *) I##name ); \
+	if ( name  ## _base) CloseLibrary( name ## _base); \
 
 
 DefineLib( UTF8 );
@@ -38,21 +40,21 @@ int main(int nargs,char **args)
 	int ret;
 	unsigned char n;
 
-	Locale_base = IExec -> OpenLibrary("locale.library", 53);
-	if (Locale_base) ILocale = (struct LocaleIFace *)  IExec -> GetInterface( Locale_base, "main", 1, TAG_END );
+	Locale_base = OpenLibrary("locale.library", 53);
+	if (Locale_base) ILocale = (struct LocaleIFace *)  GetInterface( Locale_base, "main", 1, TAG_END );
 
-	Diskfont_base = IExec -> OpenLibrary("diskfont.library", 53);
-	if (Diskfont_base) IDiskfont = (struct DiskfontIFace *)  IExec -> GetInterface( Diskfont_base, "main", 1, TAG_END );
+	Diskfont_base = OpenLibrary("diskfont.library", 53);
+	if (Diskfont_base) IDiskfont = (struct DiskfontIFace *)  GetInterface( Diskfont_base, "main", 1, TAG_END );
 
-	UTF8_base = IExec -> OpenLibrary("UTF8.library", VER);
-	if (UTF8_base) IUTF8 = (struct UTF8IFace *)  IExec -> GetInterface( UTF8_base, "main", 1, TAG_END );
+	UTF8_base = OpenLibrary("UTF8.library", VER);
+	if (UTF8_base) IUTF8 = (struct UTF8IFace *)  GetInterface( UTF8_base, "main", 1, TAG_END );
 
 	if ((ILocale)&&(IDiskfont)&&(IUTF8))
 	{
-		locale = ILocale->OpenLocale(NULL);
-		CHAR_CODES = (ULONG *) IDiskfont -> ObtainCharsetInfo(DFCS_NUMBER, (ULONG) locale -> loc_CodeSet , DFCS_MAPTABLE);
+		locale = OpenLocale(NULL);
+		CHAR_CODES = (ULONG *) ObtainCharsetInfo(DFCS_NUMBER, (ULONG) locale -> loc_CodeSet , DFCS_MAPTABLE);
 		ret = ami_main(nargs,args);
-		ILocale->CloseLocale(locale);
+		CloseLocale(locale);
 	}
 	else
 	{
@@ -75,7 +77,7 @@ void utf8_dump(unsigned char *utf8)
 	int len,ret;
 	do
 	{
-		ret = IUTF8->GetGlyph( utf8 + pos, &len) ;
+		ret = UTF8GetGlyph( utf8 + pos, &len) ;
 		pos += len;
 
 		printf("%d\n",ret);
@@ -95,19 +97,19 @@ int ami_main(int nargs,char **args)
 
 	printf(" %08x, %08x \n",org_txt , CHAR_CODES );
 
-	if (utf8 = IUTF8->Encode( CHAR_CODES, org_txt, MEMF_PRIVATE ))
+	if (utf8 = UTF8Encode( CHAR_CODES, org_txt, MEMF_PRIVATE ))
 	{
 		printf("its encoded\n");
 
 		utf8_dump(utf8);
 		printf("txt length %d\n",strlen(org_txt));
 
-		printf("utf8 length %d\n",IUTF8->Length( utf8 ));
+		printf("utf8 length %d, utf8 size %d\n",UTF8Length( utf8 ),UTF8GetSize( utf8 ) );
 
-		if (txt = IUTF8->Decode(  CHAR_CODES, utf8, MEMF_PRIVATE ))
+		if (txt = UTF8Decode(  CHAR_CODES, utf8, MEMF_PRIVATE ))
 		{
 			printf("%s\n",txt);
-			IExec -> FreeVec(txt);
+			FreeVec(txt);
 		}
 
 		if (fd = open( "ram:utf8.txt", O_CREAT | O_RDWR ))
@@ -116,7 +118,7 @@ int ami_main(int nargs,char **args)
 			close(fd);
 		}
 
-		IExec -> FreeVec(utf8);
+		FreeVec(utf8);
 	}
 }
 
