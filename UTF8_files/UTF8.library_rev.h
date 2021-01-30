246c63290dc7359d3cfaa1a889946d7af3d141b2
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2021-01-30 12:15:56 +0100

    Updated version string and readme

--- UTF8_files/UTF8.library_rev.h
+++ UTF8_files/UTF8.library_rev.h
@@ -1,6 +1,6 @@
-#define VERSION		6
+#define VERSION		7
 #define REVISION	1
-#define DATE		"14.2.2015"
-#define VERS		"UTF8.library 6.1"
-#define VSTRING		"UTF8.library 6.1 (14.2.2015)"
-#define VERSTAG		"\0$VER: UTF8.library 5.1 (14.2.2015) by Kjetil Hvalstrand"
+#define DATE		"29.1.2021"
+#define VERS		"UTF8.library 7.1"
+#define VSTRING		"UTF8.library 7.1 (29.1.2021)"
+#define VERSTAG		"\0$VER: UTF8.library 7.1 (29.1.2021) by Kjetil Hvalstrand"
